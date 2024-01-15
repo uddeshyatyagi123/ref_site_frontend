b07c83cd2adafb1499623b3cd115d6d69102d76d
uddeshya tyagi <uddeshyatyagi123@gmail.com>
2024-01-16 02:12:09 +0530

    second

---
+++ frontend/src/Components/RefreeSignUp.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './RefreeSignUp.css'
+
+function RefreeSignUp() {
+  return (
+    <>
+    <div id='background'  className='h-screen w-screen'>
+hello
+    </div>
+      
+    </>
+  )
+}
+
+export default RefreeSignUp
