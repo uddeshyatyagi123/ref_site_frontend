b4cac214882983f683214067e9c12a2a048adb35
uddeshya tyagi <uddeshyatyagi123@gmail.com>
2024-01-19 02:20:30 +0530

    refree registration form

--- frontend/src/Components/RefreeSignUp.jsx
+++ frontend/src/Components/RefreeSignUp.jsx
@@ -1,12 +1,112 @@
 import React from 'react'
-import './RefreeSignUp.css'
+// import './RefreeSignUp.css'
+import { TEInput, TERipple } from "tw-elements-react";
+import girl from "../assets/refereesignupgirl.png"
 
 function RefreeSignUp() {
   return (
     <>
-    <div id='background'  className='h-screen w-screen'>
-hello
-    </div>
+    
+ <section className="h-screen">
+      <div className="h-full">
+        {/* <!-- Left column container with background--> */}
+        <div className="g-6 flex h-full flex-wrap items-center justify-center lg:justify-between">
+          <div className="shrink-1 mb-12 grow-0 basis-auto md:mb-0 md:w-9/12 md:shrink-0 lg:w-6/12 xl:w-6/12">
+            <img
+            
+              src={girl}
+              className="w-full "
+              alt="Sample image"
+            />
+          </div>
+
+          {/* <!-- Right column container --> */}
+          <div className="mb-12 md:mb-0 md:w-8/12 lg:w-5/12 xl:w-5/12">
+            <form>
+
+
+            <TEInput
+                type="name"
+                label="Your full name"
+                size="lg"
+                className="mb-4"
+              ></TEInput>
+            <TEInput
+                type="companyname"
+                label="Company Name"
+                size="lg"
+                className="mb-4"
+              ></TEInput>
+            <TEInput
+                type="post"
+                label="Post in company"
+                size="lg"
+                className="mb-4"
+              ></TEInput>
+
+              {/* <!-- Email input --> */}
+              <TEInput
+                type="email"
+                label="Company Email verification"
+                size="lg"
+                className="mb-4"
+              ></TEInput>
+
+              {/* <!--Password input--> */}
+              <TEInput
+                type="password"
+                label="Password"
+                className="mb-4"
+                size="lg"
+              ></TEInput>
+              <TEInput
+                type="otp"
+                label="OTP"
+                className="mb-4"
+                size="lg"
+              ></TEInput>
+              
+
+              <div className="mb-6 flex items-center justify-between">
+
+ <button
+        type="button"
+        className="inline-block rounded-full bg-primary-100 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-primary-700 transition duration-150 ease-in-out hover:bg-primary-accent-100 focus:bg-primary-accent-100 focus:outline-none focus:ring-0 active:bg-primary-accent-200"
+      >
+        Send OTP
+      </button>
+
+                {/* <!--Forgot password link--> */}
+                <a href="#!">Terms and conditions</a>
+              </div>
+
+              {/* <!-- Login button --> */}
+              <div className="text-center lg:text-left">
+                <TERipple rippleColor="light">
+                  <button
+                    type="button"
+                    className="inline-block rounded bg-primary px-7 pb-2.5 pt-3 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+                  >
+                    Register
+                  </button>
+                </TERipple>
+
+                {/* <!-- Register link --> */}
+                <p className="mb-0 mt-2 pt-1 text-sm font-semibold">
+                  Have an account?{" "}
+                  <a
+                    href="#!"
+                    className="text-danger transition duration-150 ease-in-out hover:text-danger-600 focus:text-danger-600 active:text-danger-700"
+                  >
+                    Login
+                  </a>
+                </p>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
       
     </>
   )
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,6 @@
+// TW Elements is free under AGPL, with commercial license required for specific uses. See more details: https://tw-elements.com/license/ and contact us for queries at tailwind@mdbootstrap.com 
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -9,4 +10,3 @@ export default {
   },
   plugins: [],
 }
-
